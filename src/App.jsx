d63afe0054395e94ea8bfeff86f9a6c22f4b1a46
SamMelgar <samsama.melgar@gmail.com>
2024-01-31 23:48:50 -0500

    app completed with css

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,37 @@
 import { useState } from 'react'
 import { puppyList } from './data.js'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import './index.css'
 
 function App() {
-const [count, setCount] = useState(0)
-const [puppies, setPuppies] = useState(puppyList)
-console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  {()=>{console.log("puppy id: ", puppy.id)}}
+
+  function handleClick(puppyId) {
+    setFeatPupId(puppyId);
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+    console.log(featuredPup); // Log featuredPup data to the console
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        { 
-   puppies.map((puppy) => {
-     return <p>{puppy.name}</p>
-   })
-}
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="App">
+      {puppies.map((puppy) => (
+        <p key={puppy.id} onClick={() => handleClick(puppy.id)}>
+          {puppy.name}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      ))}
+      {featPupId && (
+        <div className="featured">
+          <h2 className="custom">{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App
