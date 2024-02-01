41301081ee1ac7b4a488a3c9ccea985cc459d8d8
SamMelgar <samsama.melgar@gmail.com>
2024-01-31 21:56:39 -0500

    added the array

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import { puppyList } from './data.js'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+const [count, setCount] = useState(0)
+const [puppies, setPuppies] = useState(puppyList)
+console.log(puppyList)
   return (
     <>
       <div>
@@ -15,6 +18,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        { 
+   puppies.map((puppy) => {
+     return <p>{puppy.name}</p>
+   })
+}
       </div>
       <h1>Vite + React</h1>
       <div className="card">
